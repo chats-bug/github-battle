eae96055ad0aa46621288aad2f21edd3ed8a652e
chats-bug <71580082+chats-bug@users.noreply.github.com>
2022-11-02 00:36:10 +0530

    Initial test commit

---
+++ app/components/Popular.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import PropTypes from "prop-types";
+
+function LanguagesNav({ selected, onUpdateLanguage }) {
+	const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+
+	return(
+		<select
+			onChange={(e) => onUpdateLanguage(e.target.value)}
+			selected={selected}
+		>
+			{languages.map((language) => (
+				<option key={language} value={language}>
+					{language}
+				</option>
+			))}
+		</select>
+	)
+}
+LanguagesNav.propTypes = {
+	selected: PropTypes.string.isRequired,
+	onUpdateLanguage: PropTypes.func.isRequired
+}
+
+export default class Popular extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedLanguage: "All"
+		};
+		this.updateLanguage = this.updateLanguage.bind(this);
+	}
+
+	updateLanguage(selectedLanguage) {
+		this.setState({
+			selectedLanguage
+		})
+	}
+
+	render() {
+		const { selectedLanguage } = this.state
+
+		return(
+			<main>
+				<LanguagesNav 
+					selected={selectedLanguage}
+					onUpdateLanguage={this.updateLanguage}
+				/>
+				{/* {JSON.stringify(this.state, null, 2)} */}
+			</main>
+		);
+	}
+};
